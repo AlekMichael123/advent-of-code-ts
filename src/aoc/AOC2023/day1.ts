56ae023df077cc992a749d6c3902a5def9e87dcc
AlekMichael123 <77558217+AlekMichael123@users.noreply.github.com>
2024-01-03 20:41:25 -0800

    Boom, day1 part1

--- src/aoc/AOC2023/day1.ts
+++ src/aoc/AOC2023/day1.ts
@@ -1,10 +1,21 @@
+/**
+ * https://adventofcode.com/2023/day/1
+ */
 import Day from "../day";
 
 export class Day1 implements Day {
   part1(input: string): string {
-    throw new Error("Method not implemented.");
+    return input.split("\n")
+      .map(line => line.replace(/[^0-9]/g, ""))
+      .reduce((acc, line) => {
+        if (line.length === 1) {
+          return acc + Number(line.repeat(2));
+        }
+        return acc + Number(line[0] + line[line.length - 1]);
+      }, 0)
+      .toString();
   }
   part2(input: string): string {
-    throw new Error("Method not implemented.");
+    return "";
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,13 @@ main();
 function main() {
   const input = getInput();
   const day = new AOC.AOC2023.Day1();
-  day.part1(input);
-  day.part2(input);
+  console.log("Part 1");
+  console.log(day.part1(input));
+  console.log("Part 2");
+  console.log(day.part2(input));
 }
 
 function getInput() {
   const inputPath = path.join(__dirname, "\\..\\..\\input.txt");
-  return fs.readFileSync(inputPath).toString();
+  return fs.readFileSync(inputPath).toString().trim();
 }
