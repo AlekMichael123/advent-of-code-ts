73a182dec6eddd9d1b640ef09b3ab5a7764b9b80
AlekMichael123 <77558217+AlekMichael123@users.noreply.github.com>
2024-01-03 20:59:35 -0800

    Day 1 complete

--- src/aoc/AOC2023/day1.ts
+++ src/aoc/AOC2023/day1.ts
@@ -8,6 +8,9 @@ export class Day1 implements Day {
     return input.split("\n")
       .map(line => line.replace(/[^0-9]/g, ""))
       .reduce((acc, line) => {
+        if (!line.length) {
+          return acc;
+        }
         if (line.length === 1) {
           return acc + Number(line.repeat(2));
         }
@@ -16,6 +19,39 @@ export class Day1 implements Day {
       .toString();
   }
   part2(input: string): string {
-    return "";
+    const relations: Record<string, number> = {
+      "one": 1,
+      "two": 2,
+      "three": 3,
+      "four": 4,
+      "five": 5,
+      "six": 6,
+      "seven": 7,
+      "eight": 8,
+      "nine": 9,
+    };
+    return input.split("\n")
+      .map(line => {
+        const split = line.split("");
+        const indices: [number, number][] = [];
+
+        Object.keys(relations)
+          .forEach(key => {
+            let i = line.indexOf(key);
+            while (i !== -1 && i < line.length) {
+              indices.push([i, relations[key]]);
+              i = line.indexOf(key, i + 1);
+            }
+          });
+        indices.forEach(([i, val]) => split[i] = val.toString());
+        return split.join("").replace(/[^0-9]/g, "");
+      })
+      .reduce((acc, line) => {
+        if (line.length === 1) {
+          return acc + Number(line.repeat(2));
+        }
+        return acc + Number(line[0] + line[line.length - 1]);
+      }, 0)
+      .toString();
   }
 }
