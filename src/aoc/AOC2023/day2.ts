315f9047030f3c195ba81fe648d7255cb2668316
AlekMichael123 <77558217+AlekMichael123@users.noreply.github.com>
2024-01-04 20:46:06 -0800

    day2 setup

---
+++ src/aoc/AOC2023/day2.ts
@@ -0,0 +1,10 @@
+import Day from "../day";
+
+export class Day2 implements Day {
+  part1(input: string): string {
+    throw new Error("Method not implemented.");
+  }
+  part2(input: string): string {
+    throw new Error("Method not implemented.");
+  }
+}
